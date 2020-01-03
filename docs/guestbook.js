045886014ddc2526e0b0d83c5e90c4a99ebcede2
tpdanh <49422001+tpdanh@users.noreply.github.com>
2020-01-03 15:44:18 -0600

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://5e273981.us-south.apigw.appdomain.cloud/pete-guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
